97308e0cc0ecf6554b31f7d18d25c96942b6592c
costatnsi <costa.timothy@gmail.com>
2017-03-06 20:06:26 -0800

    Updated documentation with mesh parameters corrections.

--- documentation/html/search/all_1.js
+++ documentation/html/search/all_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['bctype',['bctype',['../structedge.html#a6b2c236dcddf6e714c0cdcf8e821d7d3',1,'edge::bctype()'],['../structface.html#a3c54f0ea4e673faa5e02dc71f885d766',1,'face::bctype()']]],
   ['boundary_5fnodes',['boundary_nodes',['../structboundary__nodes.html',1,'']]],
-  ['build',['build',['../classhgf_1_1mesh_1_1voxel.html#a78d91cfd2f790abac88e6bde4b0ad111',1,'hgf::mesh::voxel::build()'],['../classhgf_1_1models_1_1uniform__porenetwork.html#ae315a847b102178fc54a17c83d8655c8',1,'hgf::models::uniform_porenetwork::build()'],['../classhgf_1_1models_1_1stokes.html#a31064021fd5e22515d37ccfa01d11b71',1,'hgf::models::stokes::build()']]],
+  ['build',['build',['../classhgf_1_1mesh_1_1voxel.html#a78d91cfd2f790abac88e6bde4b0ad111',1,'hgf::mesh::voxel::build()'],['../classhgf_1_1models_1_1uniform__porenetwork.html#ae315a847b102178fc54a17c83d8655c8',1,'hgf::models::uniform_porenetwork::build()'],['../classhgf_1_1models_1_1stokes.html#a7472059337fc1166d5aa2941cca4cafe',1,'hgf::models::stokes::build()']]],
   ['build_5funiform_5fnetwork',['build_uniform_network',['../classhgf_1_1models_1_1uniform__porenetwork.html#a6672dc6acb9e28108da4f13fd031a25f',1,'hgf::models::uniform_porenetwork']]],
   ['byibyj',['byIbyJ',['../structby_iby_j.html',1,'']]],
   ['byjbyi',['byJbyI',['../structby_jby_i.html',1,'']]],
